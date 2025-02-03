ab77d71317a5896092bbe706acc4245bd66c12d6
APIVerve Team <hello@apiverve.com>
2025-02-03 15:57:50 -0600

    Minor Version Update

---
+++ SolarPotentialQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class SolarPotentialQueryOptions {
+public number lat { get; set; }
+public number lon { get; set; }
+}
+
+}
